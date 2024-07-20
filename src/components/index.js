fb81a481c24da9643dbb259b617c010402e3579b
stormrage-web <sharfull33@gmail.com>
2024-07-20 11:55:00 +0300

    chore: deleted exceed component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
-import About from "./About";
 import Tech from "./Tech";
 import Experience from "./Experience";
 import Works from "./Works";
@@ -12,7 +11,6 @@ import CanvasLoader from "./Loader";
 export {
   Hero,
   Navbar,
-  About,
   Tech,
   Experience,
   Works,
