5e6e70f5fc612d12d2a7bbef0ee6b1f443edef78
stormrage-web <sharfull33@gmail.com>
2024-07-31 02:33:01 +0300

    chore: changed constants

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,62 +1,29 @@
 import logo from "./logo.png";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 
 import tinkoff from "./company/tinkoff.png";
 import sber from "./company/sber.png";
 import rosatom from "./company/rosatom.png";
 import freelance from "./company/freelance.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import ildanDev from "./ildan-dev.png";
+import air from "./air.png";
+import spotify from "./spotify.jpg";
 
 export {
-  logo,
-  backend,
-  creator,
-  mobile,
-  web,
-  github,
-  menu,
-  close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  tinkoff,
-  sber,
-  rosatom,
-  freelance,
-  carrent,
-  jobit,
-  tripguide,
+	logo,
+	web,
+	github,
+	menu,
+	close,
+	tinkoff,
+	sber,
+	rosatom,
+	freelance,
+	ildanDev,
+	air,
+	spotify
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,28 +1,10 @@
 import {
-	mobile,
-	backend,
-	creator,
 	web,
-	javascript,
-	typescript,
-	html,
-	css,
-	reactjs,
-	redux,
-	tailwind,
-	nodejs,
-	mongodb,
-	git,
-	figma,
-	docker,
 	sber,
 	rosatom,
 	freelance,
 	tinkoff,
-	carrent,
-	jobit,
-	tripguide,
-	threejs,
+	ildanDev, air, spotify,
 } from "../assets";
 
 export const navLinks = [
@@ -31,82 +13,8 @@ export const navLinks = [
 		title: "Experience",
 	},
 	{
-		id: "welcome",
-		title: "Welcome",
-	},
-];
-
-const services = [
-	{
-		title: "Web Developer",
-		icon: web,
-	},
-	{
-		title: "React Native Developer",
-		icon: mobile,
-	},
-	{
-		title: "Backend Developer",
-		icon: backend,
-	},
-	{
-		title: "Content Creator",
-		icon: creator,
-	},
-];
-
-const technologies = [
-	{
-		name: "HTML 5",
-		icon: html,
-	},
-	{
-		name: "CSS 3",
-		icon: css,
-	},
-	{
-		name: "JavaScript",
-		icon: javascript,
-	},
-	{
-		name: "TypeScript",
-		icon: typescript,
-	},
-	{
-		name: "React JS",
-		icon: reactjs,
-	},
-	{
-		name: "Redux Toolkit",
-		icon: redux,
-	},
-	{
-		name: "Tailwind CSS",
-		icon: tailwind,
-	},
-	{
-		name: "Node JS",
-		icon: nodejs,
-	},
-	{
-		name: "MongoDB",
-		icon: mongodb,
-	},
-	{
-		name: "Three JS",
-		icon: threejs,
-	},
-	{
-		name: "git",
-		icon: git,
-	},
-	{
-		name: "figma",
-		icon: figma,
-	},
-	{
-		name: "docker",
-		icon: docker,
+		id: "projects",
+		title: "Projects",
 	},
 ];
 
@@ -171,68 +79,68 @@ const experiences = [
 
 const projects = [
 	{
-		name: "Car Rent",
+		name: "ildan-dev",
 		description:
-			"Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+			"Simple React SPA with custom animation rendering with canvas. Includes Jest tests, 2 UI themes, i18n translation, CI/CD deploy and testing.",
 		tags: [
 			{
 				name: "react",
 				color: "blue-text-gradient",
 			},
 			{
-				name: "mongodb",
-				color: "green-text-gradient",
+				name: "jest",
+				color: "orange-text-gradient",
 			},
 			{
-				name: "tailwind",
-				color: "pink-text-gradient",
+				name: "i18n",
+				color: "gray-text-gradient",
+			},
+			{
+				name: "ci/cd",
+				color: "green-text-gradient",
 			},
 		],
-		image: carrent,
-		source_code_link: "https://github.com/",
+		image: ildanDev,
+		source_code_link: "https://github.com/stormrage-web/ildan-dev",
 	},
 	{
-		name: "Job IT",
+		name: "Aeroflot helper",
 		description:
-			"Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+			"Web application for analyzing airfare prices. The project was written for a hackathon and took 3rd place. Working with big data, drawing various graphs.",
 		tags: [
 			{
 				name: "react",
 				color: "blue-text-gradient",
 			},
 			{
-				name: "restapi",
+				name: "redux",
 				color: "green-text-gradient",
 			},
 			{
-				name: "scss",
+				name: "recharts",
 				color: "pink-text-gradient",
 			},
 		],
-		image: jobit,
+		image: air,
 		source_code_link: "https://github.com/",
 	},
 	{
-		name: "Trip Guide",
+		name: "Spotify clone",
 		description:
-			"A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+			"Simple clone of Spotify web-interface with original API. Required to login to work with it.",
 		tags: [
 			{
-				name: "nextjs",
+				name: "react",
 				color: "blue-text-gradient",
 			},
 			{
-				name: "supabase",
+				name: "hooks",
 				color: "green-text-gradient",
 			},
-			{
-				name: "css",
-				color: "pink-text-gradient",
-			},
 		],
-		image: tripguide,
+		image: spotify,
 		source_code_link: "https://github.com/",
 	},
 ];
 
-export { services, technologies, experiences, projects };
+export { experiences, projects };
